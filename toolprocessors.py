c0b3cc5edd869e5f5f1a58b7aa4215f6674f0417
Pabloprz <paperez@maldita.es>
2024-12-18 17:21:58 +0100

    remove unused prints

--- toolprocessors.py
+++ toolprocessors.py
@@ -3,7 +3,6 @@ from stopping_criteria_classes import EndCriteria
 from constrained_generation import MasterLogits
 
 def get_tool_tools(dict_of_tools, tokenizer, vocab):
-  #print('building effective tools')
   tool_tools = {}
 
   # set number of lines
@@ -22,24 +21,21 @@ def get_tool_tools(dict_of_tools, tokenizer, vocab):
   # fixed size through max and min. We add 1 to consider <|endoftext token|>
   if dict_of_tools['verse_size']:
 
-    print('controlling for verse size')
     if dict_of_tools['current_verse'] == 0:
       tool_tools['LogitsProcessors'].append(MinNewTokensLengthLogitsProcessor(dict_of_tools['input_length'], dict_of_tools['verse_size'] + 1, tokenizer.eos_token_id))
       tool_tools['LogitsProcessors'].append(ForcedEOSTokenLogitsProcessor(dict_of_tools['verse_size'] + 1, tokenizer.eos_token_id))
-      #print('forced end: ', dict_of_tools['verse_size'] + 1)
     else:
       tool_tools['LogitsProcessors'].append(MinNewTokensLengthLogitsProcessor(dict_of_tools['input_length'], dict_of_tools['verse_size'] + dict_of_tools['input_length'] + 1, tokenizer.eos_token_id))
       tool_tools['LogitsProcessors'].append(ForcedEOSTokenLogitsProcessor(dict_of_tools['verse_size'] + dict_of_tools['input_length'] + 1, tokenizer.eos_token_id))
   else:
     if dict_of_tools['current_verse'] != 0:
-      print('setting a minimum')
       tool_tools['LogitsProcessors'].append(MinNewTokensLengthLogitsProcessor(dict_of_tools['input_length'], 2, tokenizer.eos_token_id))
-      #print('forced end: ', dict_of_tools['verse_size'] + dict_of_tools['input_length'] + 1)
 
   # apply semantic, syllabic and rhyming restrictins
   if dict_of_tools['cos_sim']:
     tool_tools['LogitsProcessors'].append(MasterLogits(dict_of_tools, vocab, tokenizer))
 
+  # set end criteria
   tool_tools['StoppingCriteria'].append(EndCriteria(tokenizer.eos_token_id))
 
 
